2a30d85fdd3b1ed40b2f834b156532b0f8e446e2
Mariya M <maxa.ua@gmail.com>
2020-04-29 22:46:59 +0300

    one more language is added

--- conftest.py
+++ conftest.py
@@ -9,22 +9,10 @@ def pytest_addoption(parser):
 @pytest.fixture
 def browser(request):
     language_name = request.config.getoption("language")
-    browser = None
     options = Options()
-    if language_name == "en":
-        print("\nstart chrome browser for test..")
-        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
-        browser = webdriver.Chrome(options=options)
-    elif language_name == "es":
-        print("\nstart chrome browser for test..")
-        options.add_experimental_option('prefs', {'intl.accept_languages': "es"})
-        browser = webdriver.Chrome(options=options)
-    elif language_name == "fr":
-        print("\nstart chrome browser for test..")
-        options.add_experimental_option('prefs', {'intl.accept_languages': "fr"})
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be en, es or fr")
+    print("\nstart chrome browser for test..")
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
